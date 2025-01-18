60c15c47e2853464a0e559ca285ca257585947b5
BruceChapmanJr10 <BruceChapmanJr10@gmail.com>
2025-01-18 18:07:34 -0500

    added hero component

--- perfection-lawns/src/App.jsx
+++ perfection-lawns/src/App.jsx
@@ -1,11 +1,15 @@
 import { useState } from 'react'
 import './App.css'
 import Navbar from './components/Navbar'
+import Hero from './components/Hero'
 
 function App() {
 
   return (
-    <Navbar />
+    <div>
+      <Navbar />
+      <Hero />
+    </div>
   )
 }
 
