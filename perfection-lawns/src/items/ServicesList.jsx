8f3bdfe4f66b9c29d81e086e6e81099ee47634f0
BruceChapmanJr10 <BruceChapmanJr10@gmail.com>
2025-01-19 22:39:05 -0500

    added Service List Component

---
+++ perfection-lawns/src/items/ServicesList.jsx
@@ -0,0 +1,35 @@
+const ServicesList = [
+  {
+    id: 1,
+    title: "Lawn Mowing",
+    description: "Mowing lawns giving perfect stripes",
+  },
+  {
+    id: 2,
+    title: "Leaf Clean-up",
+    description: "Cleaning leaves off your yard making it look beautiful again",
+  },
+  {
+    id: 3,
+    title: "Landscape Maintenance",
+    description: "Keep your landscape looking perfect year round",
+  },
+  {
+    id: 4,
+    title: "Planting",
+    description: "Panting the perfect flowers to make your garden beautiful",
+  },
+  {
+    id: 5,
+    title: "Mulching",
+    description:
+      "Putting fresh mulch in your gardens beds to keep them healthy and beautiful",
+  },
+  {
+    id: 6,
+    title: "Spring Clean-ups",
+    description: "Come into spring ready to impress the neighborhood",
+  },
+];
+
+export default ServicesList;
