e97968d855f6bcdfaf45ef0a8b26b90693f07d9d
BruceChapmanJr10 <BruceChapmanJr10@gmail.com>
2025-01-19 22:34:26 -0500

    Added Navbar scroll effect

--- perfection-lawns/src/components/Navbar.jsx
+++ perfection-lawns/src/components/Navbar.jsx
@@ -1,9 +1,32 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import Button from "../items/Button";
+import { motion, useScroll, useMotionValueEvent } from "framer-motion";
 
 const Navbar = () => {
+  const [isHidden, setIsHidden] = useState(false);
+  const { scrollY } = useScroll();
+  const lastYRef = useRef(0);
+
+  useMotionValueEvent(scrollY, "change", (y) => {
+    const difference = y - lastYRef.current;
+    if (Math.abs(difference) > 50) {
+      setIsHidden(difference > 0);
+      lastYRef.current = y;
+    }
+  });
   return (
-    <nav className="bg-black text-white px-8 md:px-16 lg:px-24">
+    // Navbar Scroll Effect
+    <motion.nav
+      animate={isHidden ? "hidden" : "visable"}
+      whileHover={"visable"}
+      onFocusCapture={() => setIsHidden(false)}
+      variants={{
+        visable: { y: "0%" },
+        hidden: { y: "-90%" },
+      }}
+      transition={{ duration: 0.2 }}
+      className="bg-black text-white px-8 md:px-16 lg:px-24 sticky top-0 z-10"
+    >
       <div className="container py-2 flex justify-center md:justify-between items-center">
         <div className="text-2xl font-bold hidden md:inline">
           {" "}
@@ -28,7 +51,7 @@ const Navbar = () => {
         </div>
         <Button text="Submit" />
       </div>
-    </nav>
+    </motion.nav>
   );
 };
 
