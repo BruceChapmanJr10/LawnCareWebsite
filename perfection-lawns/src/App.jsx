cde4983b06a3cef9762aefbaa312373a85c62f41
BruceChapmanJr10 <BruceChapmanJr10@gmail.com>
2025-01-21 19:17:17 -0500

    added routes to navbar

--- perfection-lawns/src/App.jsx
+++ perfection-lawns/src/App.jsx
@@ -4,6 +4,10 @@ import Navbar from "./components/Navbar";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import LawnService from "./pages/LawnService";
+import Hero from "./components/Hero";
+import Services from "./components/Services";
+import About from "./components/About";
+import Contact from "./components/Contact";
 
 function App() {
   return (
@@ -12,6 +16,10 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/lawn-service" element={<LawnService />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/aboutowner" element={<Hero />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </div>
   );
--- perfection-lawns/src/components/Navbar.jsx
+++ perfection-lawns/src/components/Navbar.jsx
@@ -39,13 +39,13 @@ const Navbar = () => {
           <a href="/" className="hover:text-gray-400">
             Home
           </a>
-          <a href="#about" className="hover:text-gray-400">
+          <a href="/about" className="hover:text-gray-400">
             About
           </a>
-          <a href="#services" className="hover:text-gray-400">
+          <a href="/services" className="hover:text-gray-400">
             Services
           </a>
-          <a href="#contact" className="hover:text-gray-400">
+          <a href="/contact" className="hover:text-gray-400">
             Contact
           </a>
           <a href="#blog" className="hover:text-gray-400">
