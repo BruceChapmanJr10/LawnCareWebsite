583553524f4781cf131043f1c21bcd71dcb9abba
BruceChapmanJr10 <BruceChapmanJr10@gmail.com>
2025-01-24 22:01:15 -0500

    updated ul on LeafInfo component

--- perfection-lawns/src/components/LeafInfo.jsx
+++ perfection-lawns/src/components/LeafInfo.jsx
@@ -28,26 +28,25 @@ const LeafInfo = () => {
         </p>
         <ul className="text-lg list-disc marker:text-green-800 mx-auto text-left pl-5 space-y-5">
           <li>
-            <span className="font-bold">Lawn Mowing and Edging:</span>
+            <span className="font-bold">Leaf Blowing:</span>
+            <br />
             <span className="text-base">
-              We will blow all the leaves off your property
+              ** We will blow all the leaves off your property. **
             </span>
           </li>
           <li>
-            <span className="font-bold">Hedge Trimming:</span>
+            <span className="font-bold">Leaf Mulching:</span>
             <br />
             <span className="text-base">
-              ** Keep your shrubs and hedges neat and tidy with our expert
-              trimming services, enhancing the overall appearance of your yard.
-              **
+              ** We blow all leaves into the yard and mulch it up. **
             </span>
           </li>
           <li>
-            <span className="font-bold">Weed Control:</span>
+            <span className="font-bold">Leaf Removal:</span>
             <br />
             <span className="text-base">
-              ** We’ll remove unsightly weeds from your lawn and flower beds to
-              maintain a healthy and clean landscape. **
+              ** We provide thorough leaf collection, ensuring all leaves are
+              removed from your lawn, flower beds, and driveway. **
             </span>
           </li>
           <li>
@@ -67,12 +66,61 @@ const LeafInfo = () => {
             </span>
           </li>
         </ul>
-        <p className="mt-7 text-lg md:mx-12">
-          By adding maintenance to your plan, you'll not only save time, but
-          you'll also ensure your home’s exterior stays flawless throughout the
-          year. At Perfection Lawns LLC, we take pride in delivering exceptional
-          service and a landscape that reflects the care and attention it
-          deserves.
+      </div>
+      <div className="flex flex-col px-5 text-left space-y-6 mt-8">
+        <p className="mt-4 text-lg md:mx-12 text-center text-gray-300">
+          Benifits of Leaf Removal Include:
+        </p>
+        <ul className="text-lg list-disc mx-auto max-w-screen-md marker:text-green-800 text-left pl-5 space-y-5">
+          <li>
+            <span className="font-bold">Prevents Lawn Damage:</span>
+            <br />
+            <span className="text-base">
+              ** Leaving leaves on the lawn can suffocate the grass, leading to
+              fungal growth and dead spots. Regular leaf removal helps prevent
+              this damage and promotes healthy grass. **
+            </span>
+          </li>
+          <li>
+            <span className="font-bold">Improves Appeal:</span>
+            <br />
+            <span className="text-base">
+              ** A tidy lawn free of fallen leaves enhances your home's curb
+              appeal, creating a clean and well-maintained appearance throughout
+              the season. **
+            </span>
+          </li>
+          <li>
+            <span className="font-bold">Enhanced Lawn Care: </span>
+            <br />
+            <span className="text-base">
+              ** Leaf removal is a key part of lawn maintenance. Removing debris
+              helps your lawn breathe and promotes healthy growth when spring
+              arrives.**
+            </span>
+          </li>
+          <li>
+            <span className="font-bold">Comprehensive Cleanup:</span>
+            <br />
+            <span className="text-base">
+              ** Our services go beyond just leaf removal. We also clear debris
+              from hard surfaces like sidewalks, patios, and driveways, leaving
+              your entire property looking perfect. **
+            </span>
+          </li>
+          <li>
+            <span className="font-bold">Customizable Packages:</span>
+            <br />
+            <span className="text-base">
+              ** Whether you need a one-time removal or regular visits
+              throughout the fall, we offer tailored plans to meet your specific
+              needs. **
+            </span>
+          </li>
+        </ul>
+        <p className="mt-7 text-lg md:mx-12 text-center">
+          Let us help you maintain a beautiful, healthy lawn all year long with
+          our expert leaf removal services.
         </p>
       </div>
       <div className="mt-8 space-x-4 flex flex-row justify-center">
