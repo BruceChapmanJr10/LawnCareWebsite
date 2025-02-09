3433a1bb2985e7dd12e7f253a621ae872a844d83
BruceChapmanJr10 <BruceChapmanJr10@gmail.com>
2025-02-08 21:18:02 -0500

    added hash router

--- perfection-lawns/src/App.jsx
+++ perfection-lawns/src/App.jsx
@@ -13,24 +13,26 @@ import LeafCleanup from "./pages/LeafCleanup";
 import Mulching from "./pages/Mulching";
 import Planting from "./pages/Planting";
 import SpringCleanups from "./pages/SpringCleanups";
-
+import { HashRouter } from "react-router-dom";
 function App() {
   return (
     <div>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/lawn-service" element={<LawnService />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/aboutowner" element={<Hero />} />
-        <Route path="/services" element={<Services />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/landscape" element={<Landscape />} />
-        <Route path="/leafcleanup" element={<LeafCleanup />} />
-        <Route path="/mulching" element={<Mulching />} />
-        <Route path="/planting" element={<Planting />} />
-        <Route path="/springcleanups" element={<SpringCleanups />} />
-      </Routes>
+      <HashRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/lawn-service" element={<LawnService />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/aboutowner" element={<Hero />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/landscape" element={<Landscape />} />
+          <Route path="/leafcleanup" element={<LeafCleanup />} />
+          <Route path="/mulching" element={<Mulching />} />
+          <Route path="/planting" element={<Planting />} />
+          <Route path="/springcleanups" element={<SpringCleanups />} />
+        </Routes>
+      </HashRouter>
     </div>
   );
 }
